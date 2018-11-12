de09656d3fdaaa68dea41c3daff53b959b1a3c6c
AyushHariharan <ayush0624@gmail.com>
2018-11-12 10:04:54 -0500

    Started w/ TensorFlow

---
+++ TensorFlow/House Data Regression.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import, division, print_function
+
+import tensorflow as tf
+from tensorflow import keras
+
+import numpy as np  
+
+boston_housing = keras.datasets.boston_housing
+
+(train_data, train_labels), (test_data, test_labels) = boston_housing.load_data()
+
+# Shuffle the training set
+order = np.argsort(np.random.random(train_labels.shape))
+train_data = train_data[order]
+train_labels = train_labels[order]
+
