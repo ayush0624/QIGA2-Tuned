1fb1d7b9e639c9a9ff5c01f9726ee5aef57bef11
AyushHariharan <ayush0624@gmail.com>
2018-11-12 09:37:32 -0500

    Started working through tutorial

---
+++ QIGA-2/Qconfig.py
@@ -0,0 +1,23 @@
+# Before you can use the jobs API, you need to set up an access token.
+# Log in to the Quantum Experience. Under "Account", generate a personal 
+# access token. Replace 'PUT_YOUR_API_TOKEN_HERE' below with the quoted
+# token string. Uncomment the APItoken variable, and you will be ready to go.
+
+APItoken = '5b2f71479eae0159258df0ece626df4f137a6fa7126058500c086b17aa23333b244c003475c8b7f1c2c162c52fc81f2d272d300881e872cf1ba28a3060afe090'
+
+config = {
+    'url': 'https://quantumexperience.ng.bluemix.net/api',
+
+    # If you have access to IBM Q features, you also need to fill the "hub",
+    # "group", and "project" details. Replace "None" on the lines below
+    # with your details from Quantum Experience, quoting the strings, for
+    # example: 'hub': 'my_hub'
+    # You will also need to update the 'url' above, pointing it to your custom
+    # URL for IBM Q.
+    'hub': None,
+    'group': None,
+    'project': None
+}
+
+if 'APItoken' not in locals():
+    raise Exception('Please set up your access token. See Qconfig.py.')
