e20b4452b236a2c5e022cd58993bfcfaa4a70045
AYUSH HARIHARAN (812785) <812785@lcps.org>
2019-01-10 10:02:05 -0500

    Fixed DLL Issue but cant run noisy simulation

--- QIGA-2/QIGA Program.py
+++ QIGA-2/QIGA Program.py
@@ -1,21 +1,16 @@
 from qiskit import QuantumRegister, ClassicalRegister
 from qiskit import QuantumCircuit, execute, BasicAer, IBMQ
-from qiskit.providers.aer import noise
 from qiskit.tools.visualization import circuit_drawer
 from qiskit.tools.visualization import plot_histogram
-
-#create the first quantum program
-qp = QuantumProgram()
 
 # set up quantum registers
 qubit_number = 2
 register_number = 10
 
 #Create quantum population and quantum chromosomes
-registers = [qiskit.QuantumRegister(qubit_number) for i in range(register_number)]
-classicalregisters = [qiskit.ClassicalRegister(qubit_number) for i in range(register_number)]
-qc = qiskit.QuantumCircuit(*registers)
-
+registers = [QuantumRegister(qubit_number) for i in range(register_number)]
+classicalregisters = [ClassicalRegister(qubit_number) for i in range(register_number)]
+qc = QuantumCircuit(*registers)
 
 #superposition and measurement
 for qr in registers:
@@ -26,7 +21,7 @@ for qr in registers:
 
 #determining probability amplitudes        
 simulator = BasicAer.backends(name='statevector_simulator')[0]
-results = qp.execute(qc, simulator, shots=1024)
+results = execute(qc, simulator, shots=1024)
 result = job.result()
 result.get_statevector()
 
@@ -39,7 +34,7 @@ def knapsack():
 APItoken = '5b2f71479eae0159258df0ece626df4f137a6fa7126058500c086b17aa23333b244c003475c8b7f1c2c162c52fc81f2d272d300881e872cf1ba28a3060afe090'
 url = 'https://quantumexperience.ng.bluemix.net/api'
 
-IBMQ.enable_account(APItoken, url=url)
+IBMQ.save_account(APItoken, url=url)
 IBMQ.load_accounts()
 realBackend = IBMQ.backends(name='ibmqx2')[0]
 device = IBMQ.get_backend(realBackend)
