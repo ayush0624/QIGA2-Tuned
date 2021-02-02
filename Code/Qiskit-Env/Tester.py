a2e3e0af59d1f53e4788f4eb65370116e7ce0f47
ayush0624 <31373700+ayush0624@users.noreply.github.com>
2021-02-02 09:28:08 -0800

    Updated organization structure

---
+++ Code/Qiskit-Env/Tester.py
@@ -0,0 +1,46 @@
+from qiskit import QuantumProgram
+import Qconfig
+
+qp = QuantumProgram()
+qp.set_api(Qconfig.APItoken, Qconfig.config["url"]) # set the APIToken and API url
+
+# set up registers and program
+qr = qp.create_quantum_register('qr', 16)
+cr = qp.create_classical_register('cr', 16)
+qc = qp.create_circuit('simons algorithm', [qr], [cr])
+
+
+def create_dict(simons_string):
+    dict = {
+        '00': simons_string,
+        '01': simons_string + 1,
+        simons_string: '00'
+        simons_string + 1: '01'
+    }
+
+    return  dict
+
+
+data = create_dict('10')
+
+qc.h(qr)
+
+def mygate():
+    pass
+
+
+mygate()
+
+qc.h(qr)
+
+
+qc.barrier(qr)
+
+# measure
+for j in range(16):
+    qc.measure(qr[j], cr[j])
+
+# run and get results
+results = qp.execute(["smiley_writer"], backend='ibmqx5', shots=1024)
+stats = results.get_counts("smiley_writer")
+
